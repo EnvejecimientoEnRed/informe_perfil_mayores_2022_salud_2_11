d7ff2f97e08518290fb99d7d646651353a2d41b2
Carlos Muñoz Díaz <47306703G@cchs.csic.es>
2022-03-17 13:42:05 +0100

    Nuevo cambio de datos

--- pre/js/charts/charts.js
+++ pre/js/charts/charts.js
@@ -23,7 +23,54 @@ export function initChart(iframe) {
     //Lectura de datos
     d3.csv('https://raw.githubusercontent.com/CarlosMunozDiazCSIC/informe_perfil_mayores_2022_salud_2_11/main/data/edades_quinquenales_salud_percibida_v2.csv', function(error,data) {
         if (error) throw error;
-        console.log(data);
+        
+        let dataFiltered = data.filter(function(item) {
+            if (item.Edad != 'TOTAL' && item.Sexo != 'Ambos sexos') {
+                return item;
+            }
+        });
+
+        let margin = {top: 10, right: 10, bottom: 50, left: 30},
+            width = document.getElementById('chart').clientWidth - margin.left - margin.right,
+            height = document.getElementById('chart').clientHeight - margin.top - margin.bottom;
+
+        let svg = d3.select("#chart")
+            .append("svg")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+                .attr("transform",
+                    "translate(" + margin.left + "," + margin.top + ")");
+
+        let x0 = d3.scaleBand()
+            .domain(d3.map(dataFiltered, function(d){ return d.Edad; }).keys())
+            .range([ 0, width ]);            
+            
+        let x1 = d3.scaleBand()
+            .domain(['H','M'])
+            .rangeRound([0, x0.bandwidth()])
+            .padding(0.2);
+
+        svg.append("g")
+            .attr("transform", "translate(0," + (height + 25) + ")")
+            .call(d3.axisBottom(x0));
+
+        for(let i = 0; i < 8; i++) {
+            svg.append("g")
+                .attr("transform", "translate(" + x0.bandwidth() * i  + "," + (height + 0) + ")")
+                .call(d3.axisBottom(x1));   
+        }        
+    
+        // Add Y axis
+        let y = d3.scaleLinear()
+            .domain([0, 100])
+            .range([ height, 0 ]);
+        svg.append("g")
+            .call(d3.axisLeft(y));
+
+        let z = d3.scaleOrdinal()
+            .range([COLOR_PRIMARY_2, COLOR_PRIMARY_1, COLOR_GREY_1, COLOR_COMP_2, COLOR_COMP_1]);
+        
 
         function init() {
 
